14c53de342f98666afc8b37ae2933a94fa549b93
OmarMorillon <132393229+OmarMorillon@users.noreply.github.com>
2025-02-19 14:21:03 -0600

    Add files via upload

---
+++ Practica1.sql
@@ -0,0 +1,25 @@
+/*Sistema Gestor De Base de Datos
+Alumno:Rocio Arely Mata Gamboa 
+       Eldi Balderrama Velasquez 
+Grado: 8°C */
+
+USE AdventureWorks2022;
+GO
+
+---EJERCICIO 1---
+SELECT Name
+FROM Production.Product
+WHERE ListPrice > 1000;
+
+---Ejercicio 2---
+SELECT PostalCode
+FROM Person.Address
+WHERE PostalCode LIKE '95%';
+
+---Ejercicio 3---
+SELECT COUNT(*)
+FROM Production.Product
+WHERE Size = 'A';
+
+
+
