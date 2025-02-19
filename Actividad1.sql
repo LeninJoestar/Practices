9f55ba6e38b7af004efdbda74ba0606c062796f8
erika445 <84411194+erika445@users.noreply.github.com>
2025-02-19 14:09:12 -0600

    Actividad en clase

---
+++ Actividad1.sql
@@ -0,0 +1,27 @@
+USE AdventureWorks2022;
+GO
+--Ejercicio 1: Obtener los productos con un precio mayor a 1000
+--Instrucción: Muestra los nombres de los productos cuyo precio unitario es mayor a 1000, de la tabla Production.Product.
+SELECT Name 
+FROM Production.Product 
+WHERE ListPrice > 1000
+
+--Ejercicio 2: Listar los códigos postales que empiezan con '98'
+--Instrucción: Muestra todos los códigos postales de la tabla Person.Address que comiencen con '98'.
+SELECT PostalCode 
+FROM Person.Address
+WHERE PostalCode LIKE '98%';
+
+--Ejercicio 3: Contar cuántos productos tienen un tamaño específico
+--Instrucción: Cuenta cuántos productos tienen un tamaño específico (por ejemplo, "M") en la tabla Production.Product
+SELECT * 
+FROM  Production.Product
+WHERE Size LIKE 'M'
+
+--Ejercicio 4: Obtener la venta total más alta
+--Instrucción: Encuentra la venta más alta (por monto total) registrada en la tabla Sales.SalesOrderHeader.
+SELECT MAX(TotalDue)
+AS venta_mas_alta  
+FROM Sales.SalesOrderHeader 
+--Ejercicio 5: Contar cuántos productos tienen stock
+--Instrucción: Muestra cuántos productos tienen un StockLevel mayor a 0 en la tabla Production.Product.
