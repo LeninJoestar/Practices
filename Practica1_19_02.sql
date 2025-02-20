be2b7be3c83ce4d5e88f86744fcef5247e8c473b
Eldinosaurio30 <eldi.balderrama.21isc@tecsanpedro.edu.mx>
2025-02-20 12:09:22 -0600

    Update Practica1_19_02.sql

--- Practica1_19_02.sql
+++ Practica1_19_02.sql
@@ -1,53 +1,49 @@
 /*Sistema Gestor De Base de Datos
 Alumno:Rocio Arely Mata Gamboa 
-       Eldi Balderrama Velasquez 
+       Eldi Guadalupe Balderrama Velazquez 
 Grado: 8°C */
 
-USE AdventureWorks2022;
+USE AdventureWorks2022
 GO
 
 ---EJERCICIO 1---
 SELECT Name
 FROM Production.Product
-WHERE ListPrice > 1000;
+WHERE ListPrice > 1000
 
 ---Ejercicio 2---
 SELECT PostalCode
 FROM Person.Address
-WHERE PostalCode LIKE '95%';
+WHERE PostalCode LIKE '98%'
 
 ---Ejercicio 3---
 SELECT COUNT(*)
 FROM Production.Product
-WHERE Size = 'A';
+WHERE Size = 'M'
 
----Ejercicio 3---
-SELECT COUNT(*)
-FROM Production.Product
-WHERE Size = 'A';
 
 ---Ejercicio 4---
 SELECT MAX(TotalDue) AS VentaMasAlta
-FROM Sales.SalesOrderHeader;
+FROM Sales.SalesOrderHeader
 
 ---Ejercicio 5--- 
 SELECT COUNT(*) AS ProductosConStock
 FROM Production.Product
-WHERE SafetyStockLevel > 0;
+WHERE SafetyStockLevel > 0
 
 ---Ejercicio 6---
-SELECT TOP 5 p.Name AS Producto, SUM(sod.OrderQty) AS CantidadVendida
+SELECT TOP 5 p.Name AS Producto SUM(sod.OrderQty) AS CantidadVendida
 FROM Sales.SalesOrderDetail sod
 JOIN Production.Product p ON sod.ProductID = p.ProductID
 GROUP BY p.Name
-ORDER BY CantidadVendida DESC;
+ORDER BY CantidadVendida DESC
 
 ---Ejercicio 7---
-SELECT c.CustomerID, SUM(soh.TotalDue) AS TotalVentas
+SELECT c.CustomerID SUM(soh.TotalDue) AS TotalVentas
 FROM Sales.SalesOrderHeader soh
 JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
 GROUP BY c.CustomerID
-ORDER BY TotalVentas DESC;
+ORDER BY TotalVentas DESC
 
 
 
