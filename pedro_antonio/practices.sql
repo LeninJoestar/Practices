5f6f4de820edc2e4040e94bee15475d281ffe12a
PedroAntLeos <132860690+PedroAntLeos@users.noreply.github.com>
2025-02-24 13:17:09 -0600

    Update practices.sql

--- pedro_antonio/practices.sql
+++ pedro_antonio/practices.sql
@@ -19,3 +19,19 @@ SELECT DISTINCT PostalCode FROM Person.Address WHERE PostalCode LIKE '98%';
 /*Ejercicio 3: Contar cuántos productos tienen un tamaño específico
 Instrucción: Cuenta cuántos productos tienen un tamaño específico (por ejemplo, "M") en la tabla Production.Product.*/
 SELECT COUNT(*) FROM Production.Product WHERE Size = 'M';
+
+USE AdventureWorks2017
+
+/*Ejercicio 4: Obtener la venta total más alta
+Instrucción: Encuentra la venta más alta (por monto total) registrada en la tabla Sales.SalesOrderHeader.*/
+SELECT MAX(TotalDue) AS 'Venta más alta' FROM Sales.SalesOrderHeader
+
+/*Ejercicio 5: Contar cuántos productos tienen stock
+Instrucción: Muestra cuántos productos tienen un StockLevel mayor a 0 en la tabla Production.Product.*/
+SELECT *FROM Production.Product WHERE SafetyStockLevel > 1
+
+/*Ejercicio 6: Productos más vendidos
+Instrucción: Obtén los 5 productos más vendidos según la cantidad.
+
+Pista: Usa las tablas Sales.SalesOrderDetail (detalles de las órdenes de venta) y Production.Product (productos).*/
+SELECT TOP 5 *FROM Sales.SalesOrderDetail ORDER BY OrderQty DESC
